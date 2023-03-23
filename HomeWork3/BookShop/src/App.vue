c97b53ec289438977aa05856aeac68da39a6d654
Koemtola <koemtola1216@gmail.com>
2023-03-23 07:49:14 +0700

    uploaded

---
+++ HomeWork3/BookShop/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HomePage from './components/HomePage.vue' 
+</script>
+<template>
+  <HomePage />
+</template>
+<style scoped>
+</style>
