3861709a0077d82a373e43b6f6c41e3938631d5b
koemTola <koemtola1216@gmail.com>
2023-07-07 22:26:06 +0700

    deploy

--- BookShop/vite.config.js
+++ BookShop/vite.config.js
@@ -13,8 +13,17 @@ export default defineConfig({
   }
 
 })
-  module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/ip_cours/'
-      : '/'
-  }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ip_cours/'
+    : '/',
+    
+}
+// vite.config.js
+const vuePlugin = require('@vitejs/plugin-vue')
+
+module.exports = {
+  plugins: [
+    vuePlugin()
+  ]
+}
