3ed70d66f91b7337d30d11707cc1742da2958236
koemTola <koemtola1216@gmail.com>
2023-07-07 23:19:08 +0700

    adding deploy

--- BookShop/vite.config.js
+++ BookShop/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bookshop/',
+  base: '/ip_cours/',
   plugins: [vue()],
   resolve: {
     alias: {
