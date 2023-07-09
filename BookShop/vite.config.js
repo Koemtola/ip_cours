981f5781dc787c00f8b7e4c50c4ea46e4f810640
koemTola <koemtola1216@gmail.com>
2023-07-09 12:16:55 +0700

    updated

--- BookShop/vite.config.js
+++ BookShop/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ip_cours',
+  base: '/ip_cours/BookShop',
   plugins: [vue()],
   resolve: {
     alias: {
